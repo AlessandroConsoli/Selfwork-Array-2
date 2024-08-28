0bc4f43f7c3faf96a30bd26db7b2a70626966949
Alessandro Consoli <alessandroconsoli.1982@gmail.com>
2024-08-28 16:34:39 +0200

    completato

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 function mediaCalculator(...n) {
     let media = n.reduce((tot, el)=> tot + el) / n.length;
-    console.log(media + n.filter((n)=> n < media));    
+    console.log(media);
+    let minoriDiMedia = n.filter((n)=> n < media);
+    console.log(minoriDiMedia);
+    let maggioriDiMedia = n.filter((n)=> n > media);
+    console.log(maggioriDiMedia);
+        
 }
 
 mediaCalculator(3, 5, 10, 2, 8);
+
+
+
+// Appunto per la Q&A:   è possibile inserire una stringa preimpostata prima dell'array risultante?
 
